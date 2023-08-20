a3c3f7e8a05d84afaa4dfcc88b646d06590c030e
Luis Alejandro <luisalejandromedinab@gmail.com>
2023-08-20 11:24:58 -0500

    Tabla usuarios

--- index.js
+++ index.js
@@ -14,19 +14,41 @@ const pool = new pg.Pool({
   //ssl: true,
 });
 
-app.listen(3000) // lo que decimos es que se va a ejecutar en el puerto 3000
+app.listen(3000); // lo que decimos es que se va a ejecutar en el puerto 3000
 
-app.get('/', (req, res) => { //Cuando el usuario entre a la ruta principal, le vamos a responder con un mensaje
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  //Cuando el usuario entre a la ruta principal, le vamos a responder con un mensaje
+  res.send("Hello World");
+});
 
 app.get("/ping", async (req, res) => {
   //Cuando el usuario entre a la ruta ping, le vamos a responder con un mensaje
 
   //hacemos una consulta de la fecha actual de la base de datos
-    const result = await pool.query("SELECT NOW()")
+  const result = await pool.query("SELECT NOW()");
 
   return res.json(result.rows[0]);
 });
 
-console.log('Server on port', 3000)
+//Funciones para crear tablas
+app.get("/create-tables", async (req, res) => {
+  try {
+    const query = `
+    CREATE TABLE IF NOT EXISTS usuarios (
+    uidUsuario SERIAL PRIMARY KEY,
+    imagen VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    contraseña VARCHAR(255) NOT NULL,
+    esAdministrador BOOLEAN NOT NULL DEFAULT FALSE
+);
+    `;
+    await pool.query(query);
+    res.send("Tabla usuarios creada");
+  } catch (error) {
+    console.error("Error al crear la tabla:", error);
+    res.status(500).send("Error al crear la tabla");
+  }
+});
+
+console.log("Server on port", 3000);
